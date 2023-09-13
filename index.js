6587ef244a32fc4eb648f92166f603e5c0c4974f
Egor <96371791+Zrossiz@users.noreply.github.com>
2023-09-13 17:37:56 +0300

    upd: homework 5

--- index.js
+++ index.js
@@ -1,43 +1,25 @@
-const { createInterface } = require("readline");
-const path = require("path");
-const fs = require("fs");
+import http from "http";
+import { createInterface } from "readline";
 
 const readline = createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-const file = path.resolve(__dirname, "history.json");
-
-const game = () => {
-  const variants = ["Орел", "Решка"];
-  const rundomNumber = Math.round(Math.random());
-
-  readline.question(
-    'Игра в "Орел или решка началась, попробуйте угадать"',
-    (userAnswer) => {
-      const StringUserAnswer = String(userAnswer);
-      const resultOfGame = {
-        "Правильный ответ": variants[rundomNumber],
-        "Ваш ответ": StringUserAnswer,
-      };
-      if (StringUserAnswer == variants[rundomNumber]) {
-        console.log("Поздравляем, вы отгадали");
-        console.log("Историю своих игр можете посмотреть в файлу history.txt");
-        fs.appendFile(file, JSON.stringify(resultOfGame), (err) => {
-          readline.close();
-        });
-        readline.close();
-      } else {
-        console.log("К сожалению вы проиграли");
-        console.log("Историю своих игр можете посмотреть в файлу history.txt");
-        fs.appendFile(file, JSON.stringify(resultOfGame), (err) => {
-          readline.close();
-        });
-        readline.close();
-      }
-    }
-  );
+const start = () => {
+  readline.question("Введите нужный город(En) ", (userAnswer) => {
+    const url = `http://api.weatherstack.com/forecast?access_key=${process.env.TOKEN}&query=${userAnswer}`;
+    http.get(url, (res) => {
+      res.setEncoding("utf8");
+      let rowData = "";
+      res.on("data", (chunk) => (rowData += chunk));
+      res.on("end", () => {
+        let parseData = JSON.parse(rowData);
+        console.log(parseData);
+      });
+    });
+    readline.close();
+  });
 };
 
-game();
+start();
